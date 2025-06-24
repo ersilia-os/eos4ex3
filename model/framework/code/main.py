0697caa0ba188270f0a1cace94cfe5927540afdc
GemmaTuron <gemma@ersilia.io>
2025-06-24 12:15:12 +0200

    fix input file

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -2,8 +2,9 @@
 import os
 import csv
 import sys
-import yaml
 import subprocess
+import tempfile
+import shutil
 import pandas as pd
 
 # parse arguments
@@ -13,11 +14,27 @@ output_file = sys.argv[2]
 # current file directory
 root = os.path.dirname(os.path.abspath(__file__))
 
+temp_dir = tempfile.mkdtemp(prefix="ersilia_", dir=root)
+input_file_ = os.path.join(temp_dir, "normalized_input.csv")
+
+# read SMILES from .csv file, assuming one column with header
+with open(input_file, "r") as f:
+    reader = csv.reader(f)
+    next(reader)  # skip header
+    smiles_list = [r[0] for r in reader]
+
+with open(input_file_, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["smiles"])
+    for s in smiles_list:
+        writer.writerow([s])
+
+
 # my model
 subprocess.run(["python", os.path.join(root, "gather_representation.py"),
 	"--gpu", "cpu",
 	"--output_filepath", output_file,
-	"--smiles_filepath", input_file,
+	"--smiles_filepath", input_file_,
 	"--smiles_colname", "smiles",
 	"--chemid_colname", "smiles",
 	"--representation", "gin_concat_R1000_E8000_lambda0.0001"], 
@@ -25,7 +42,7 @@ subprocess.run(["python", os.path.join(root, "gather_representation.py"),
 
 
 # Compare SMILES input and output
-smiles_input = pd.read_csv(os.path.join(input_file), sep=',')['smiles'].tolist()
+smiles_input = pd.read_csv(os.path.join(input_file_), sep=',')['smiles'].tolist()
 smiles_output = pd.read_csv(os.path.join(output_file), sep=',').iloc[:, 0].tolist()
 assert smiles_input == smiles_output
 
@@ -34,3 +51,5 @@ output = pd.read_csv(os.path.join(output_file), sep=',')
 output = output.iloc[:, 1:]
 output.columns = [f"dim_{i:03d}" for i in range(len(output.columns))]
 output.to_csv(output_file, sep=',', index=False)
+
+shutil.rmtree(temp_dir)
